6616c5ab2ef732b375ac5a12a9c6d2988fe00e1d
Farzaneh Sefidabi <fsefidabi@gmail.com>
2021-01-10 09:43:24 +0330

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navbar/>
+  <Navbar/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import Navbar from '@/components/Navbar'
+
+  export default {
+    name: 'App',
+    components: {
+      Navbar
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>navbar</div>
+</template>
+
+<script>
+  export default {
+    name: 'Navbar'
+  }
+</script>
+
+<style scoped>
+
+</style>
