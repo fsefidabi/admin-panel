d6994f14e7fbdde6bc5e3671110c276a471bfbd9
Farzaneh Sefidabi <fsefidabi@gmail.com>
2021-01-10 21:50:05 +0330

    completing dashboard

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,58 @@
 <template>
-  <Navbar/>
-  <Navbar/>
+  <div class="flex p-6">
+    <Navbar :nav-items="navItems" :current-tab="state.currentTab" :update-current-tab="updateCurrentTab"/>
+    <div class="w-11/12 py-6 px-10 bg-gray-100 rounded-2xl">
+      <StatusBar class="mb-5"/>
+      <div class="mt-4 my-9 border-b border-gray-800 border-opacity-20"></div>
+      <router-view :current-tab="state.currentTab"/>
+    </div>
+  </div>
 </template>
 
 <script>
-
   import Navbar from '@/components/Navbar'
+  import {reactive} from 'vue'
+  import StatusBar from '@/components/StatusBar'
 
   export default {
-    name: 'App',
-    components: {
-      Navbar
+    components: {StatusBar, Navbar},
+    setup () {
+      const navItems = [
+        {
+          name: 'Dashboard',
+          icon: 'fa fa-home'
+        },
+        {
+          name: 'Pages',
+          icon: 'fa fa-file'
+        },
+        {
+          name: 'Users',
+          icon: 'fa fa-users'
+        },
+        {
+          name: 'Archive',
+          icon: 'fa fa-archive'
+        }
+      ]
+
+      const state = reactive({
+        currentTab: navItems[0].name
+      })
+
+      function updateCurrentTab (item) {
+        state.currentTab = item.name
+      }
+
+      return {
+        navItems,
+        state,
+        updateCurrentTab
+      }
     }
   }
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
+
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,23 @@
 <template>
-  <div>navbar</div>
+  <nav class="w-1/12 lg:w-1/6 text-left">
+    <a href="#" class="inline-block mt-5 ml-7 text-2xl text-white font-bold">
+      <i class="fa fa-diamond" aria-hidden="true"></i>
+      Logo
+    </a>
+    <div class="w-11/12 my-6 border-b border-gray-50 border-opacity-20"></div>
+    <NavItem :nav-items="navItems" :current-tab="currentTab" :update-current-tab="updateCurrentTab"/>
+  </nav>
 </template>
 
 <script>
+  import NavItem from '@/components/NavItem'
+
   export default {
-    name: 'Navbar'
+    name: 'Navbar',
+    components: {NavItem},
+    props: ['navItems', 'currentTab', 'updateCurrentTab']
   }
+
 </script>
 
 <style scoped>
