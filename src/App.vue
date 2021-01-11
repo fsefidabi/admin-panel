e6996e0e6288a502d3993bed2fc7ec864e79a281
Farzaneh Sefidabi <fsefidabi@gmail.com>
2021-01-11 21:25:03 +0330

    adding vuex

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,31 @@
 <template>
   <div class="w-screen flex flex-col md:flex-row justify-center mx-auto p-6">
-    <Navbar :nav-items="navItems" :current-tab="state.currentTab" :update-current-tab="updateCurrentTab"/>
-    <MainPanel :current-tab="state.currentTab"/>
+    <Navbar :nav-items="navItems" :current-tab="tab" :update-current-tab="updateCurrentTab"/>
+    <MainPanel :current-tab="tab"/>
   </div>
 </template>
 
 <script>
+  import { useStore } from 'vuex'
+  import { computed } from 'vue'
   import Navbar from '@/components/Navbar'
-  import {reactive, ref} from 'vue'
   import MainPanel from '@/views/MainPanel'
 
   export default {
     components: {MainPanel, Navbar},
     setup () {
-      const navItems = [
-        {
-          name: 'Dashboard',
-          icon: 'fa fa-home'
-        },
-        {
-          name: 'Pages',
-          icon: 'fa fa-file'
-        },
-        {
-          name: 'Users',
-          icon: 'fa fa-users'
-        },
-        {
-          name: 'Archive',
-          icon: 'fa fa-archive'
-        }
-      ]
+      const store = useStore()
+      const tab = computed(() => store.state.currentTab)
+      const navItems = store.state.navItems
 
-      const state = reactive({
-        currentTab: ref('Dashboard')
-      })
-
-      function updateCurrentTab (item) {
-        state.currentTab = item.name
-        console.log(state.currentTab)
+      async function updateCurrentTab (item) {
+        await store.dispatch('updateCurrentTab', item)
+        console.log(tab)
       }
 
       return {
+        tab,
         navItems,
-        state,
         updateCurrentTab
       }
     }
